01355ecb96f59bb406211e891529e18a6f54b720
yoryi <yoryiojeda@outlook.com>
2023-10-20 01:21:05 -0500

    Feat: integration renders main view

--- app/screen/datatable/index.js
+++ app/screen/datatable/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
+import {Table, Row, Rows} from 'react-native-table-component';
 
 //Components
 import styles from './styles';
@@ -7,15 +8,45 @@ import {COLORS} from '../../constants';
 import {StatusBar} from '../../components';
 
 function DataTable() {
-  const renderSearch = () => {};
-  const renderFilters = () => {};
-  const renderDataTable = () => {};
+  const tableHead = ['Head', 'Head2', 'Head3', 'Head4'];
+  const tableData = [
+    ['1', '2', '3', '4'],
+    ['a', 'b', 'c', 'd'],
+    ['1', '2', '3', '456\n789'],
+    ['a', 'b', 'c', 'd'],
+    ['a', 'b', 'c', 'd'],
+    ['a', 'b', 'c', 'd'],
+    ['a', 'b', 'c', 'd'],
+  ];
+
+  const renderSearch = () => (
+    <View>
+      <Text>Search</Text>
+    </View>
+  );
+
+  const renderFilters = () => (
+    <View>
+      <Text>Filtros</Text>
+    </View>
+  );
+
+  const renderDataTable = () => (
+    <View>
+      <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+        <Row data={tableHead} />
+        <Rows data={tableData} />
+      </Table>
+    </View>
+  );
 
   const renderUI = () => (
     <View style={styles.wrapper}>
       <SafeAreaView>
         <StatusBar mode={'dark'} backgroundColor={COLORS.WHITE} />
-        <Text>Data Table</Text>
+        {renderSearch()}
+        {renderFilters()}
+        {renderDataTable()}
       </SafeAreaView>
     </View>
   );
